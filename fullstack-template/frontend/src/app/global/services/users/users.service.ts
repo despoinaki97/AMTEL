3d56d0b625b8d8916fcdd918fcffa39e1d689579
Dimitra3617 <dimitraktistaki@hotmail.com>
2021-12-22 15:39:16 +0200

    pull

---
+++ fullstack-template/frontend/src/app/global/services/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { User } from 'src/app/Objects/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  user1:User;
+  user2:User;
+  user3:User;
+  currphoto = '../../../assets/Despoina.jpg';
+  currphoto2= '../../../assets/dimitra.jpg';
+  currphoto3= '../../../assets/HC.jpg';
+  
+  constructor() {
+    this.user1=new User(1,this.currphoto,"Despoina");
+    this.user2=new User(2,this.currphoto2,"Dimitra");
+    this.user3=new User(3,this.currphoto3,"Abdul");
+   }
+
+
+}
